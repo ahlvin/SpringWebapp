0a2ee21d76a10a01ec6cba9776de502d1938f2e9
ahlvin <42684180+ahlvin@users.noreply.github.com>
2020-11-09 10:43:38 -0500

    Initial commit

---
+++ Spring5Webapp/src/main/java/guru/springframework/Spring5Webapp/domain/Book.java
@@ -0,0 +1,84 @@
+package guru.springframework.Spring5Webapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+
+    private String title;
+    private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        authors = authors;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return Id == book.Id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (Id ^ (Id >>> 32));
+    }
+}
---
+++ Spring5Webapp/src/test/java/guru/springframework/Spring5Webapp/Spring5WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.Spring5Webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
